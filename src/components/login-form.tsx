c2eabb881209be62d7c0faa156c8771432f3f2c2
mohamed <mohamed@MacBook-Pro-de-mohamed.local>
2020-05-11 02:14:48 +0200

    fix build errors

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -1,5 +1,4 @@
-import React, { useState, useEffect } from 'react';
-import PropTypes from 'prop-types';
+import React from 'react';
 import LoadingButton from './loading-button';
 
 interface Props {
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './assets/styles/index.scss';
-import Main from './layouts/main/main';
+// import Main from './layouts/main/main';
 import Auth from './layouts/auth/auth';
 
 ReactDOM.render(
