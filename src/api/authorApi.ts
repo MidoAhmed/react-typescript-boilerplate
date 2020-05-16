053a1e11c32e0fdc3e355f8a6e339d63e7370484
mohamed <mohamed@MacBook-Pro-de-mohamed.local>
2020-05-16 06:34:52 +0200

    add mock api

---
+++ src/api/authorApi.ts
@@ -0,0 +1,24 @@
+import { handleResponse, handleError } from './apiUtils';
+const baseUrl = process.env.REACT_APP_API_URL + '/authors/';
+
+export function getAuthors() {
+  return fetch(baseUrl)
+    .then(handleResponse)
+    .catch(handleError);
+}
+
+export function saveAuthor(author) {
+  return fetch(baseUrl + (author.id || ''), {
+    method: author.id ? 'PUT' : 'POST', // POST for create, PUT to update when id already exists.
+    headers: { 'content-type': 'application/json' },
+    body: JSON.stringify(author),
+  })
+    .then(handleResponse)
+    .catch(handleError);
+}
+
+export function deleteAuthor(authorId) {
+  return fetch(baseUrl + authorId, { method: 'DELETE' })
+    .then(handleResponse)
+    .catch(handleError);
+}
